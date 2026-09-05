4b8091f03a32a047d2deb31f04ece7b210783a33
Mohamed Boulbab <boulbab.mohamed487@gmail.com>
2026-09-05 15:48:05 +0100

    coliaty integration

--- app/pgs/hr-shamp/page.tsx
+++ app/pgs/hr-shamp/page.tsx
@@ -9,8 +9,8 @@ const AUDIO_REVIEWS = [
 ]
 
 const PRODUCT_SKU = "18SGTM"
-const PRICE_1 = 99
-const PRICE_2 = 183
+const PRICE_1 = 115
+const PRICE_2 = 195
 const SAVING = PRICE_1 * 2 - PRICE_2
 
 const FAQ = [
